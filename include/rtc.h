ff80c73f8754e42b71bca5298cd68026c36b46bc
bmys <bmys94@gmail.com>
2019-11-20 02:01:32 +0100

    Create LSIinit, RTCinit, setTimeStampBuffer functions

--- include/rtc.h
+++ include/rtc.h
@@ -1,4 +1,13 @@
 #ifndef _RTC_H
 #define _RTC_H
 
+#include <stm32f3xx_ll_rtc.h>
+#include <stm32f3xx_ll_rcc.h>
+#include "stm32f3xx_ll_pwr.h"
+#include <stm32f3xx_ll_bus.h>
+
+void LSIinit();
+void RTCinit();
+void setTimeStampBuffer(char *time_buffer);
+
 #endif //_RTC_H
