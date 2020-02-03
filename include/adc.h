cc98594225ba2d23d2e11777a04d6e90fb50b16b
bmys <bmys94@gmail.com>
2020-02-03 01:19:28 +0100

    Add ADC interruption, Calculate AVG ADC Value, Refactor

--- include/adc.h
+++ include/adc.h
@@ -1,12 +1,12 @@
 #ifndef _ADC_H
 #define _ADC_H
 
+#include "utils.h"
+#include <display.h>
+#include <led.h>
 #include <stm32f3xx_ll_adc.h>
 #include <stm32f3xx_ll_gpio.h>
 #include <stm32f3xx_ll_rcc.h>
-#include <led.h>
-#include <display.h>
-#include "utils.h"
 
 #define ENABLE_TEMPERATURE_INPUT_GPIO LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA)
 #define TEMPERATURE_PORT GPIOC
@@ -14,8 +14,13 @@
 #define SUPPLY_VOLTAGE 3.0f
 #define ADC_RESOLUTION 4095.0f
 #define RE_RESISTOR 2000.0f
+#define SUPPLY_VOLTAGE 3.0f
+#define SAMPLES_NUMBER 1000
 
+extern short ADC_AVG_VALUE;
 
 void adcInit();
 void analogInputInit();
+void startConversion();
+
 #endif
--- src/adc.c
+++ src/adc.c
@@ -1,6 +1,18 @@
 #include "adc.h"
 #include "utils.h"
 #include <math.h>
+#include <stm32f3xx_ll_exti.h>
+
+short ADC_AVG_VALUE = -1;
+
+static inline void adcCalibrate()
+{
+    LL_ADC_EnableInternalRegulator(ADC1);
+    delay(LL_ADC_DELAY_INTERNAL_REGUL_STAB_US);
+    LL_ADC_StartCalibration(ADC1, LL_ADC_DIFFERENTIAL_ENDED);
+    while (LL_ADC_IsCalibrationOnGoing(ADC1))
+        ;
+}
 
 void adcInit()
 {
@@ -10,9 +22,7 @@ void adcInit()
                                    .LowPowerMode = LL_ADC_LP_MODE_NONE };
 
     if (!LL_ADC_Init(ADC1, &adcInit)) {
-        ledWindMillToggleCycle();
-    } else {
-        led2ToggleCycle();
+        //    ERROR HANDLING
     }
 
     LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_ADC12);
@@ -26,29 +36,42 @@ void adcInit()
     LL_ADC_REG_SetSequencerRanks(ADC1, LL_ADC_REG_RANK_1, LL_ADC_CHANNEL_8);
     LL_ADC_SetChannelSamplingTime(ADC1, LL_ADC_CHANNEL_8, LL_ADC_SAMPLINGTIME_181CYCLES_5);
 
-    delay(10000000);
+    // delay(10000000);
     LL_ADC_REG_SetTriggerSource(ADC1, LL_ADC_REG_TRIG_SOFTWARE);
     LL_ADC_REG_SetContinuousMode(ADC1, LL_ADC_REG_CONV_CONTINUOUS);
     LL_ADC_REG_SetDMATransfer(ADC1, LL_ADC_REG_DMA_TRANSFER_NONE);
 
     LL_ADC_REG_SetOverrun(ADC1, LL_ADC_REG_OVR_DATA_OVERWRITTEN);
 
-    LL_ADC_EnableInternalRegulator(ADC1);
-    delay(1000000);
-
-    LL_ADC_StartCalibration(ADC1, LL_ADC_DIFFERENTIAL_ENDED);
-    while (LL_ADC_IsCalibrationOnGoing(ADC1)) {
-        led3ToggleCycle();
-    }
+    adcCalibrate();
 
     LL_ADC_Enable(ADC1);
-    delay(100000);
+    while (!LL_ADC_IsActiveFlag_ADRDY(ADC1))
+        ;
 
-    if (LL_ADC_IsEnabled(ADC1)) {
-        ledWindMillToggleCycle();
-    } else {
-        led1ToggleCycle();
+    if (!LL_ADC_IsEnabled(ADC1)) {
+        //    ERROR HANDLING
     }
+
+    LL_ADC_EnableIT_EOS(ADC1);
+    NVIC_EnableIRQ(ADC1_IRQn);
+}
+
+void ADC1_IRQHandler()
+{
+    static unsigned short counter;
+    static unsigned long acumulator;
+
+    if (counter == SAMPLES_NUMBER) {
+        ADC_AVG_VALUE = (short)((float)acumulator / (float)SAMPLES_NUMBER);
+        counter = 0;
+        acumulator = 0;
+    } else {
+        acumulator += LL_ADC_REG_ReadConversionData12(ADC1);
+        ++counter;
+    }
+
+    LL_ADC_ClearFlag_EOS(ADC1);
 }
 
 void startConversion()
@@ -56,40 +79,60 @@ void startConversion()
     if (LL_ADC_IsEnabled(ADC1) == 1) {
         LL_ADC_REG_StartConversion(ADC1);
     } else {
-        led1ToggleCycle();
-        led2ToggleCycle();
-        led1ToggleCycle();
+        //    ERROR HANDLING
     }
+}
 
-    while (LL_ADC_IsActiveFlag_EOC(ADC1)) {
-        led2ToggleCycle();
-        led3ToggleCycle();
+static inline void printResistance(char* buffer, unsigned int resistance, uint8_t line)
+{
+    snprintf(buffer, 11, "R: %u", resistance);
+    displayPuts(0, line, buffer, 0);
+}
+
+static inline void printVolt(char* buffer, float adcVoltage, uint8_t line)
+{
+    static int adcVoltageFields[2];
+    splitFloat(adcVoltage, adcVoltageFields, 2);
+    snprintf(buffer, 11, "Volt: %u.%u", adcVoltageFields[0], adcVoltageFields[1]);
+    displayPuts(0, line, buffer, 0);
+}
+
+static inline void printAdc(char* buffer, short adcValue, uint8_t line)
+{
+    if (adcValue == -1) {
+        snprintf(buffer, 11, "ADC: NOT RDY");
+    } else {
+        snprintf(buffer, 11, "ADC: %u", (unsigned int)adcValue);
     }
+    displayPuts(0, line, buffer, 0);
+}
+
+static inline void printPercentage(char* buffer, unsigned int percent, uint8_t line)
+{
+    snprintf(buffer, 11, "Perc: %u%%", (unsigned int)percent);
+    displayPuts(0, 5, buffer, 0);
 }
 
 void printADC()
 {
-    int16_t adcValue = LL_ADC_REG_ReadConversionData12(ADC1);
+    static char buffer[15] = {};
 
-    float vsense = (SUPPLY_VOLTAGE * adcValue) / ADC_RESOLUTION;
+    if (ADC_AVG_VALUE == -1) {
+        snprintf(buffer, 15, "ADC: NOT READY");
+        displayPuts(0, 3, buffer, 0);
+    } else {
+        float adcVoltage = (SUPPLY_VOLTAGE * ADC_AVG_VALUE) / ADC_RESOLUTION;
 
-    float percent = (adcValue / ADC_RESOLUTION) * 100.0f;
+        float percent = (ADC_AVG_VALUE / ADC_RESOLUTION) * 100.0f;
 
-    unsigned int rx_resistor =
-      (unsigned int)(RE_RESISTOR / (ADC_RESOLUTION / (float)adcValue - 1.0f));
+        unsigned int termistorResistance =
+          (unsigned int)(RE_RESISTOR / (ADC_RESOLUTION / (float)ADC_AVG_VALUE - 1.0f));
 
-    int vSenseFields[2];
-    splitFloat(vsense, vSenseFields, 2);
-
-    static char adc[11] = {};
-    snprintf(adc, 11, "R: %u", rx_resistor);
-    displayPuts(0, 2, adc, 0);
-    snprintf(adc, 11, "Volt: %u.%u", vSenseFields[0], vSenseFields[1]);
-    displayPuts(0, 3, adc, 0);
-    snprintf(adc, 11, "ADC: %u", (unsigned int)adcValue);
-    displayPuts(0, 4, adc, 0);
-    snprintf(adc, 11, "Perc: %u%%", (unsigned int)percent);
-    displayPuts(0, 5, adc, 0);
+        printResistance(buffer, termistorResistance, 2);
+        printVolt(buffer, adcVoltage, 3);
+        printAdc(buffer, ADC_AVG_VALUE, 4);
+        printPercentage(buffer, percent, 5);
+    }
 }
 
 void analogInputInit()
